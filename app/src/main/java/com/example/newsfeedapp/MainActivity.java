c7e18c969e3a8620807395ec96b63345c5464be8
“bryan” <bryan.dov.bergman@gmail.com>
2020-11-29 18:14:36 +0200

    got jsons back for every article. Need to store them to database for viewerArticleActivity to access.

--- app/src/main/java/com/example/newsfeedapp/MainActivity.java
+++ app/src/main/java/com/example/newsfeedapp/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,8 +24,9 @@ public class MainActivity extends AppCompatActivity {
     ListView myListView;
     ArrayAdapter<String> arrayAdapter;
     ArrayList<String> newsItems = new ArrayList<>();
-    ArrayList<String> articleCodes = new ArrayList<>();
+    ArrayList<String> jsonList = new ArrayList<>();
 
+    MyCodeGetter myCodeGetter;
     MyAPIGetter myAPIGetter;
 
 
@@ -35,9 +35,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setUpListView();
+        getAPIs();
+    }
+
+    private void getAPIs() {
+        myCodeGetter = new MyCodeGetter();
+        myCodeGetter.execute(" https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+    }
+
+    private void setUpListView() {
+
         myListView = (ListView) findViewById(R.id.myListView);
-        myAPIGetter = new MyAPIGetter();
-        myAPIGetter.execute(" https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+
+        //just to see something before getting APIs
         newsItems.add("Example Article");
 
         arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, newsItems);
@@ -52,7 +63,74 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public class MyAPIGetter extends AsyncTask<String, Void, String> {
+    public class MyAPIGetter extends AsyncTask<String, Void, ArrayList<String>> {
+        ArrayList<String> result = new ArrayList<>();
+        String resultString = "";
+        URL url;
+        HttpURLConnection urlConnection = null;
+
+        @Override
+        protected ArrayList<String> doInBackground(String... urls) {
+
+            try {
+//                System.out.println("doInBackground()");
+                for (int i = 0; i < urls.length; i++) {
+
+                    url = new URL(urls[i]);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream in = urlConnection.getInputStream();
+                    InputStreamReader reader = new InputStreamReader(in);
+                    int data = reader.read();
+
+                    while (data != -1) {
+                        char current = (char) data;
+                        resultString += current;
+                        data = reader.read();
+                    }
+
+                    if(i == 0) {
+                        Log.i("RESULT STRING = ", resultString);
+                        result.add(resultString);
+                    }
+
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                ArrayList<String> failedList = new ArrayList<>();
+                failedList.add("failed");
+                return failedList;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> s) {
+            super.onPostExecute(s);
+
+//            System.out.println("==============API CODE============== " + s);
+//            try {
+//                JSONObject jsonObject = new JSONObject(s);
+//                String title = jsonObject.getString("title");
+//                String url = jsonObject.getString("url");
+//                Log.i("title123", title);
+//                Log.i("url123", url);
+//
+//
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+
+
+
+
+
+    public class MyCodeGetter extends AsyncTask<String, Void, String> {
 
         String result = "";
         URL url;
@@ -74,8 +152,6 @@ public class MainActivity extends AppCompatActivity {
                     result += current;
                     data = reader.read();
                 }
-
-
                 return result;
 
 
@@ -90,48 +166,21 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
-            s = s.substring(1, s.length() -1);
-            Log.i("JSON", s);
+            s = s.trim();
+            s = s.substring(1, s.length() - 1);
+//            Log.i("JSON", s);
             String[] arr = s.split(",");
             String code;
+            String[] articleURLs = new String[arr.length];
 
             for (int i = 0; i < arr.length; i++) {
-               code = arr[i].trim();
-               articleCodes.add(code);
+                code = arr[i].trim();
+                articleURLs[i] = "https://hacker-news.firebaseio.com/v0/item/"+ code +".json?print=pretty";
+//                Log.i("urlList", articleURLs[i]);
             }
 
-//            System.out.println(s);
-
-            System.out.println("onPostExecute()");
-////
-//            try {
-//                JSONObject jsonObject = new JSONObject(s);
-////                String weatherInfo = jsonObject.getString("weather");
-////                Log.i("weather info", weatherInfo);
-//                JSONArray array = new JSONArray(jsonObject);
-//
-////                String articleCode;
-//
-//                for (int i = 0; i < array.length(); i++) {
-//                    JSONObject jsonPart = array.getJSONObject(i);
-//                    Log.i("code123", jsonPart.toString());
-////                    Log.i("description", jsonPart.getString("description"));
-//
-////                    main = jsonPart.getString("main");
-////                    description = jsonPart.getString("description");
-////
-////                    System.out.println("is this working " + main + " " + description);
-//
-//
-                }
-//
-//                //here where to do something with the finished code
-////                myMultiLine.setText(weatherDesc);
-//
-
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//            }
-//        }
+            myAPIGetter = new MyAPIGetter();
+            myAPIGetter.execute(articleURLs);
+        }
     }
 }
